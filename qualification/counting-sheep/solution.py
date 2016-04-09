858f5c25dbd193593ea67fc80079531bcfb7eadd
Arjan de Pooter <mail@arjandepooter.nl>
2016-04-09 15:38:39 +0200

    Added editorconfig

--- qualification/counting-sheep/solution.py
+++ qualification/counting-sheep/solution.py
@@ -3,23 +3,23 @@
 import sys
 
 def solve(n):
-	if n == 0:
-		return "INSOMNIA"
+    if n == 0:
+        return "INSOMNIA"
 
-	found = {}
-	i = 0
+    found = {}
+    i = 0
 
-	while len(found.items()) < 10:
-		x = str(n * (i + 1))
-		for c in x:
-			found[c] = True
-		i += 1
+    while len(found.items()) < 10:
+        x = str(n * (i + 1))
+        for c in x:
+            found[c] = True
+        i += 1
 
-	return i * n
+    return i * n
 
 if __name__ == '__main__':
-	n = int(sys.stdin.readline())
+    n = int(sys.stdin.readline())
 
-	for i in xrange(n):
-		N = int(sys.stdin.readline())
-		print "Case #%d: %s" % (i + 1, solve(N))
+    for i in xrange(n):
+        N = int(sys.stdin.readline())
+        print "Case #%d: %s" % (i + 1, solve(N))
